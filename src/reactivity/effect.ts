4b121d68c00b36589899e0823b5f71947c54bafa
刘勇 <viscode@163.com>
2022-02-16 16:13:01 +0800

    实现effect&reactive&依赖收集&触发依赖

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,13 +1,66 @@
+// 通过target对象存储的一个Map格式数据
+const targetMap = new Map()
+// 当前的effet，就是当前ReactiveEffect的实例对象
+let activeEffect
+
+/**
+ * effect对象，所有的effect实例都是基于此。
+ */
 class ReactiveEffect {
-  private _fn;
-  constructor (fn: () => any) {
-    this._fn = fn
+  private _fn:any; //存储class传进来的fn
+  constructor (fn) {
+    this._fn = fn  //将class传入的fn赋值给_fn
   }
   public run () {
-    this._fn()
+    activeEffect = this  //将当前实例effect赋值给activeEffect
+    this._fn() //执行class传入的fn
   }
 }
-export function effect (fn) {
+
+/**
+ * 依赖收集
+ * @param target 
+ * @param key 
+ */
+export const track = (target, key) => {
+  // target -> key -> dep
+  // 通过target从targetMap中查找对应的depsMap
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    // 如果不存在该target的depsMap便创建一个空的Map并赋值给targetMap的target属性
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+  // 获取当前key的dep
+  let dep = depsMap.get(key)
+  if (!dep) {
+    // 如果不存在，便创建一个空的set并赋值给depsMap
+    dep = new Set()
+    depsMap.set(key, dep)
+  }
+  // 将当前的effect对象存入dep
+  dep.add(activeEffect)
+}
+
+/**
+ * 触发依赖
+ * @param target 
+ * @param key 
+ */
+export const trigger = (target, key) => {
+  console.log(target, '改变了', key, Reflect.get(target, key))
+  let depsMap = targetMap.get(target)
+  let dep = depsMap.get(key)
+  for (const effect of dep) {
+    effect.run()
+  }
+}
+
+/**
+ * 
+ * @param fn 函数类型
+ */
+ export function effect (fn) {
   const _effect = new ReactiveEffect(fn)
   _effect.run()
 }
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,13 +1,27 @@
+import { track, trigger } from "./effect"
+
+/**
+ * 创建响应式对象
+ * @param raw 对象，数组，其他
+ * @returns proxy
+ */
 export const reactive = (raw) => {
   return new Proxy(raw, {
     get (target, key) {
       const res = Reflect.get(target, key)
       //依赖收集
+      track(target, key)
+      console.log(res, '获取了该对象的', target, key)
+      // 直接返回target中key的值
       return res
     },
     set (target, key, value) {
+      // 设置target中key的值为value
       const res = Reflect.set(target, key, value)
       //触发依赖
+      console.log('触发了set方法', target, key, value)
+      trigger(target, key)
+      // 返回boolean
       return res
     }
   })
