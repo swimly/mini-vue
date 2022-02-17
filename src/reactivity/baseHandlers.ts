8cb80dd32fdf022a9a074eb42d91b5e58fc126c7
刘勇 <viscode@163.com>
2022-02-17 11:14:51 +0800

    readonly&reactive嵌套

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,5 +1,6 @@
+import { isObject } from "../shared"
 import { track, trigger } from "./effect"
-import { ReactiveFlags } from "./reactive"
+import { reactive, ReactiveFlags, readonly } from "./reactive"
 
 // 缓存，避免重复执行
 const get = createGetter()
@@ -15,6 +16,10 @@ function createGetter (isReadonly = false) {
       return isReadonly
     }
     const res = Reflect.get(target, key)
+    // 如果res是object，继续转换reactive
+    if (isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res)
+    }
     //依赖收集
     if (!isReadonly) {
       track(target, key)
