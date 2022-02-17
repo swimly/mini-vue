11e1c9776c6a98c57014c805060373d9565489a7
刘勇 <viscode@163.com>
2022-02-17 15:48:10 +0800

    proxyRefs实现

--- src/reactivity/index.ts
+++ src/reactivity/index.ts
@@ -1,3 +1,3 @@
 export {effect, stop} from './effect'
 export {reactive, readonly, isReactive, isReadonly, shallowReadonly, isProxy} from './reactive'
-export {ref, isRef, unRef} from './ref'
+export {ref, isRef, unRef, proxyRefs} from './ref'
